94f8b32c06b12fceec14e43bd7537554433b2b67
shipumaruf <shipumaruf9@gmail.com>
2024-11-03 20:56:20 +0600

    added menu option

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import NavBar from './component/NavBar/NavBar'
+// import Daisy from './component/DaisyNav/Daisy'
+
+function App() {
+ 
+
+  return (
+    <>
+      {/* <Daisy></Daisy> */}
+      <NavBar></NavBar>
+      <h1 className='text-3xl bg-slate-400'>Vite + React</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
